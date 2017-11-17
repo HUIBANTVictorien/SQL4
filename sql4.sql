2c83068ddceba65556e203966950a88d3a71ddc6
Victorien HUIBANT <huibant.victorien@gmail.com>
2017-11-17 12:08:05 +0100

    ajout sql partie4

---
+++ sql4.sql
@@ -0,0 +1,26 @@
+Exercice 1
+
+Dans la table languages insérez les données suivantes :
+
+    JavaScript version 5
+    PHP version 5.2
+    PHP version 5.4
+    HTML version 5.1
+    JavaScript version 6
+    JavaScript version 7
+    JavaScript version 8
+    PHP version 7
+
+Commande en ligne de commande linux : - USE webDevelopment;
+                                      - INSERT INTO languages VALUES (1, 'JavaScript', 5), (NULL, 'PHP', 5.2), (NULL, 'PHP', 5.4), (NULL, 'HTML', 5.1), (NULL, 'JavaScript', 6), (NULL, 'JavaScript', 7), (NULL, 'JavaScript', 8), (NULL, 'PHP', 7);
+
+Exercice 2
+
+Dans la table frameworks insérez les données suivantes :
+
+    Symfony version 2.8
+    Symfony version 3
+    Jquery version 1.6
+    Jquery version 2.10
+
+Commande en ligne de commande linux : - INSERT INTO frameworks VALUES (1, 'Symfony', 2.8), (NULL, 'Symfony', 3), (NULL, 'jQuery', 1.6), (NULL, 'jQuery', 2.10);
